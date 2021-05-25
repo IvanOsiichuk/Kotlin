87a185cbcd812c4946a1f93698c32c7b7090216b
fre_ee_nergy <48798654+IvanOsiichuk@users.noreply.github.com>
2021-05-25 19:02:09 +0300

    Delete task1Probability.kt

--- task1Probability.kt
+++
@@ -1,6 +0,0 @@
-/** Частота появи символів у тексті */
-fun charProbability(text: String): Map<Char, Float> {
-    val probs = mutableMapOf<Char, Float>() // вірогідності
-    text.forEach { c -> probs[c] = probs.getOrDefault(c, 0.0F) + 1 }
-    return probs.mapValues { (_, v) -> v / text.length}
-}
